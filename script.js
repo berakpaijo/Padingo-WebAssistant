70b032b5c63a2da0073473581d8b72c10414af0e
berakpaijo <iyanmaumakan.56@gmail.com>
2024-11-18 09:11:08 +0700

    main

--- script.js
+++ script.js
@@ -138,6 +138,12 @@ function speakThis(message) {
         speech.text = finalText;
     }
 
+    else if (message.includes('open virtual class')) {
+        window.open("https://virtualclass.smpm12gkb.sch.id/my/");
+        const finalText = "Having some courses, huh?";
+        speech.text = finalText;
+    }
+
     else if (message.includes('what is') || message.includes('who is') || message.includes('what are')) {
         window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
         const finalText = "This is what i found on internet regarding " + message;
